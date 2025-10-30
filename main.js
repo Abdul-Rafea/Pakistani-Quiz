e4816503e0734984dfd2b79f46fb47d944f4feef
Abdul Rafea <abdulrafea97@gmail.com>
2025-10-31 01:09:55 +0500

    Add files via upload
    fixed some shadows and added some workign functionally for the quiz

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
-document.getElementById('refresh-button').addEventListener("click", function() {
+function refresh(){
     location.reload();
-});
-
+}
 function goto(id){
     const section = document.getElementById(id);
     if (section) {
@@ -10,7 +9,19 @@ function goto(id){
         });
         }
     else {
-        console.warn('got(): element not found with id',id);
-        
+        console.warn('got(): element not found with id',id); 
     }
     }
+
+function question1(){
+    const element1 = document.getElementById('option-button-container');
+    element1.style.display = 'flex';
+    element1.style.animation = 'pop_in 100ms linear 1';
+    const element2 = document.getElementById('quiz-overlay-buttons');
+    element2.style.display = 'flex';
+    element2.style.animation = 'pop_in 100ms linear 1'
+    const element3 = document.getElementById('begin-quiz');
+    element3.style.display = 'none';
+    const element4 = document.getElementById('question-box');
+    element4.style.animation = 'pop_in 100ms linear 1';
+}
